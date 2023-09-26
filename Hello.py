5577198e859699be0d82a2f609103ded4395298a
Karoline <98568686+kjchrz03@users.noreply.github.com>
2023-09-26 22:12:27 +0000

    returned to original content

--- Hello.py
+++ Hello.py
@@ -1,30 +1,8 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-#from streamlit.logger import get_logger
+import pandas as pd
 import numpy as np
 
-#LOGGER = get_logger(__name__)
-
 st.set_page_config(page_title="Check This Data", page_icon="🏒", initial_sidebar_state="expanded")
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
 
 
 ##########################################
@@ -32,23 +10,13 @@ def run():
 ##########################################
 
 st.title("Check This Data")
-st.markdown('''##### <span style="color:gray">Explore Player Data</span>
+st.markdown('''##### <span style="color:gray">Explore NHL Advanced Stats, Simply</span>
             ''', unsafe_allow_html=True)
                 
-tab_player, tab_team, tab_explore, tab_faq = st.tabs(["Home","Player Report Cards", "Shots Heatmap", "FAQ"])
+tab_player, tab_team, tab_explore, tab_faq = st.tabs(["Player Lookup", "Team Lookup", "Explore", "FAQ"])
 
-col1, col2, col3 = st.sidebar.columns([1,8,1])
-with col1:
-    st.write("")
-with col2:
-    st.image('figures/heroguy.png',  use_column_width=True)
-with col3:
-    st.write("")
+
 
 st.sidebar.markdown(" ## About Check This Data")
-st.sidebar.markdown("I love hockey and I love data, so I built this app to visualize some of my favorite advanced metrics to help users visualize them."  )              
-st.sidebar.info("Read more about my process on Github [Github](https://github.com/kjchrz03/hockey-streamlit).", icon="ℹ️")
-
-
-if __name__ == "__main__":
-    run()
+st.sidebar.markdown("Dip your toes into advanced hockey analytics with some of my favorite metrics"  )              
+st.sidebar.info("Read more about how the model works and see the code on my [Github](https://github.com/kjchrz03/hockey-streamlit).", icon="ℹ️")
