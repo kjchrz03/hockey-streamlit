015903387c5ea767d16c04f604374875a4bdb2f9
Karoline <98568686+kjchrz03@users.noreply.github.com>
2023-10-06 21:57:02 +0000

    updating player table

--- Hello.py
+++ Hello.py
@@ -18,6 +18,8 @@ players_df = load_players()
 
 cols = ['Name','Position','Team','Goals']
 
+# CSS for tables
+
 hide_table_row_index = """
             <style>
             thead tr th:first-child {display:none}
@@ -54,6 +56,7 @@ def color_surplusvalue(val):
 heading_properties = [('font-size', '16px'),('text-align', 'center'),
                       ('color', 'black'),  ('font-weight', 'bold'),
                       ('background', 'mediumturquoise'),('border', '1.2px solid')]
+
 cell_properties = [('font-size', '16px'),('text-align', 'center')]
 
 dfstyle = [{"selector": "th", "props": heading_properties},
@@ -65,7 +68,7 @@ st.markdown(
     """
 <style>
 .streamlit-expanderHeader {
-   font-weight: bold;
+ #   font-weight: bold;
     background: aliceblue;
     font-size: 18px;
 }
@@ -99,34 +102,22 @@ tab_player, tab_team, tab_explore, tab_faq = st.tabs(["Player Lookup", "Team Loo
 with tab_player:
     player = st.selectbox("Choose a player (or click below and start typing):", players_df.Name, index =508)
     
-    #player_pos = players_df[players_df.Name == player].Pos.to_list()[0]
-    #player_sal_class_predict = dfplayers[dfplayers.Name == player].Sal_class_predict.to_list()[0]
-    #player_max_proba = dfplayers[dfplayers.Name == player].Max_proba.to_list()[0]          
-    #player_salary = dfplayers[dfplayers.Name == player]['Salary ($M)'].to_list()[0]
+    player_position = players_df[players_df.Name == player].Position.to_list()[0]
     player_goals = players_df['goals']
-    #if player_salary == '<2':
-    #    player_salary = '<$2M'
-    #else:
-    #    player_salary = '$' +  player_salary + 'M'   
-    #player_marketvalue = dfplayers[dfplayers.Name == player]['Market Value ($M)'].to_list()[0]
-    #if player_marketvalue == '30+':
-    #    player_marketvalue = '$30M+'
-    #else:
-    #    player_marketvalue = '$' +  player_marketvalue + 'M'
-    #player_url = 'https://www.basketball-reference.com' + dfplayers[dfplayers.Name == player]['ID'].to_list()[0]                   
     
-    #st.write(f'''
-         ##### <div style="text-align: center"> In the 2021-22 NBA season, <span style="color:blue">[{player}]({player_url})</span> earned a salary of <span style="color:blue"> {player_salary}   </span> </div>
+    st.write(f'''
+         ##### <div style="text-align: center"> In the 2021-22 NBA season, <span style="color:blue">[{player}]</span> earned a salary of <span style="color:blue"> {player_goals}   </span> </div>
          
-          ##### <div style="text-align: center"> According to our model, his market value was <span style="color:blue">{player_marketvalue}</span> </div>
-     #    ''', unsafe_allow_html=True)
+          
+         ''', unsafe_allow_html=True)
     
-    #styler_player = (players_df[players_df.Name == player][cols]
-    #               .style.set_properties(**{'background': 'azure', 'border': '1.2px solid'})
-    #               .hide(axis='index')
-    #               .set_table_styles(dfstyle)
-    #               .applymap(color_surplusvalue, subset=pd.IndexSlice[:, ['Surplus Value ($M)']]))
-    #st.table(styler_player)
+    styler_player = (players_df[players_df.Name == player][cols]
+                   .style.set_properties(**{'background': 'azure', 'border': '1.2px solid'})
+                   .hide(axis='index')
+                   .set_table_styles(dfstyle)
+                  # .applymap(color_surplusvalue, subset=pd.IndexSlice[:, ['Surplus Value ($M)']])
+                  )
+    st.table(styler_player)
     
     
    # st.markdown('''#### Most Similar Players:''', unsafe_allow_html=True)
@@ -151,3 +142,32 @@ with tab_player:
 #and Win Shares (WS), all scraped from [Basketball Reference](http://www.basketball-reference.com).  
 
 #The seven market value buckets used were:  \$0-5M, \$5-10M, \$10-15M, \$15-20M, \$20-25M, \$25-30M, and \$30M+.  In keeping with best data science practices, the model was trained and fine-tuned on player data from previous years and was not exposed to any data from the 2021-22 NBA season before generating these predictions.''')
+
+##########################################
+## Team Tab                             ##
+##########################################    
+    
+with tab_team:
+    team = st.selectbox("Choose a team (or click below and start typing):", dfteams.Team, index=1)
+   
+    styler_team = (dfplayers[dfplayers.Team == team_to_tm[team]][cols].style
+                          .set_properties(**{'background': 'azure', 'border': '1.2px solid'})
+                          .hide(axis='index')
+                          .set_table_styles(dfstyle)
+                          .applymap(color_surplusvalue, subset=pd.IndexSlice[:, ['Surplus Value ($M)']])                                                    )
+    st.table(styler_team)
+    
+    st.success('''**A Brief Note on Methods:**  
+
+The machine learning model deployed in this app is a Random Forest 
+Classifier that uses the following information to predict a player's market value: Games Played, Games Started, 
+Minutes Per Game, Points Per Game, Usage Percentage, Offensive Box Plus/Minus (OBPM), Value Over Replacement Player (VORP), 
+and Win Shares (WS), all scraped from [Basketball Reference](http://www.basketball-reference.com).  
+
+The seven market value buckets used were:  \$0-5M, \$5-10M, \$10-15M, \$15-20M, \$20-25M, \$25-30M, and \$30M+.  In keeping with best data science practices, the model was trained and fine-tuned on player data from previous years and was not exposed to any data from the 2021-22 NBA season before generating these predictions.''')
+
+    
+##########################################
+## Explore Tab                          ##
+##########################################
+   
