248017372f845496509bba4447ae50c1f93a01ae
Karoline <98568686+kjchrz03@users.noreply.github.com>
2023-10-07 20:08:38 +0000

    bold headers

--- Hello.py
+++ Hello.py
@@ -134,10 +134,10 @@ with tab_player:
     st.write(f'''
         <table style="background: azure; border: 1.2px solid; width: 100%">
         <tr>
-            <td>Name</td>
-            <td>Position</td>
-            <td>Team</td>
-            <td>Goals</td>
+            <td class="bold-data">Name</td>
+            <td class="bold-data">Position</td>
+            <td class="bold-data">Team</td>
+            <td class="bold-data">Goals</td>
         </tr>
         <tr>
             <td>{players_df.loc[players_df.Name == player, 'Name'].values[0]}</td>
