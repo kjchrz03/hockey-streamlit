ec8ce02a73fb4a6034d689ce32d8049c464cb221
Karoline <98568686+kjchrz03@users.noreply.github.com>
2023-10-07 19:59:09 +0000

    table test

--- Hello.py
+++ Hello.py
@@ -131,23 +131,12 @@ with tab_player:
     selected_columns = ['Name', 'Position', 'Team', 'Goals']  # Replace with your actual column names
 
     # Create an HTML table with desired styling
-    st.write(f'''
-    <table style="background: azure; border: 1.2px solid; width: 100%">
-    <tr>
-        <th>Name</th>
-        <th>Position</th>
-        <th>Team</th>
-        <th>Goals</th>
-    </tr>
-    <tr>
-        <td>{players_df.loc[players_df.Name == player, 'Name'].values[0]}</td>
-        <td>{players_df.loc[players_df.Name == player, 'Position'].values[0]}</td>
-        <td>{players_df.loc[players_df.Name == player, 'Team'].values[0]}</td>
-        <td>{players_df.loc[players_df.Name == player, 'Goals'].values[0]}</td>
-    </tr>
-</table>
-''', unsafe_allow_html=True)
+    html_table = players_df[players_df.Name == player][selected_columns].to_html(
+        escape=False, classes='styled-table'
+    )
 
+    # Display the HTML table in Streamlit
+    st.write(html_table, unsafe_allow_html=True)
 
     # Display the HTML table in Streamlit
 #st.write(html_table, unsafe_allow_html=True)
