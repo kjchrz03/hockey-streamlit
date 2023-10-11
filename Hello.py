69edbdfec0e3ec8e59c0958778716f21bce9ffb8
Karoline <98568686+kjchrz03@users.noreply.github.com>
2023-10-11 12:57:00 +0000

    matplotlib

--- Hello.py
+++ Hello.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # Use the Agg backend
 import matplotlib.pyplot as plt
 
 import subprocess
