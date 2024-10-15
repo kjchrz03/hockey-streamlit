3ae615722391024640e189535b027ed8e1ad374f
Karoline <98568686+kjchrz03@users.noreply.github.com>
2024-10-15 10:01:07 +0000

    reveal pips

--- Hello.py
+++ Hello.py
@@ -1,10 +1,10 @@
 
-# subprocess.run(["pip", "install", "pytz"])
-# subprocess.run(["pip", "install", "nest_asyncio"])
-# subprocess.run(["pip", "install", "aiohttp"])
+subprocess.run(["pip", "install", "pytz"])
+subprocess.run(["pip", "install", "nest_asyncio"])
+subprocess.run(["pip", "install", "aiohttp"])
 
-# Install the library from the GitHub repository using pip within your Streamlit app
-# subprocess.run(["pip", "install", "git+https://github.com/the-bucketless/hockey_rink.git"])
+Install the library from the GitHub repository using pip within your Streamlit app
+subprocess.run(["pip", "install", "git+https://github.com/the-bucketless/hockey_rink.git"])
 
 # Import the external library
 import streamlit as st
@@ -238,6 +238,7 @@ with tab_bug:
 ## Player Tab                           ##
 ##########################################
 with tab_player:
+    @st.cache_data(show_spinner=True) 
     def season_data():
         try:
             # Call the function from season_data.py
